dd6f52b37effc460297ab751d9b6538a3f27fd75
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-02-12 10:32:26 -0800

    Greatly simplify anisble vault include

--- 91-ansible-vault.mk
+++ 91-ansible-vault.mk
@@ -1,18 +1,15 @@
 VAULT_VARS_FILE?=inventory/group_vars/all
 VAULT_PASSWORD_FILE?=secret/vault_password
 
-.PHONY: vault_encrypt vault_decrypt check_vault_enc check_vault_dec
+.PHONY: vault_encrypt vault_decrypt
 
-vault_encrypt: check_vault_enc
+vault_encrypt: $(VAULT_VARS_FILE)
 	ansible-vault encrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $(VAULT_VARS_FILE)
 	mv $(VAULT_VARS_FILE) $(VAULT_VARS_FILE).enc
 
-vault_decrypt: check_vault_dec
+vault_decrypt: $(VAULT_VARS_FILE).enc
 	ansible-vault decrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $(VAULT_VARS_FILE).enc
 	mv $(VAULT_VARS_FILE).enc $(VAULT_VARS_FILE)
 
-check_vault_dec:
-	test -f $(VAULT_VARS_FILE).enc
-
-check_vault_enc:
-	test -f $(VAULT_VARS_FILE)
+$(VAULT_VARS_FILE)%:
+	test -f $@
