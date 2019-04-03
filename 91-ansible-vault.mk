3c32543de7ca293dbdec1538581bd052442ee786
Svyatoslav I. Maslennikov <me@smaslennikov.com>
2019-04-03 12:05:18 -0700

    Refactor Ansible Vault targets to handle multiple files

--- 91-ansible-vault.mk
+++ 91-ansible-vault.mk
@@ -1,18 +1,21 @@
-VAULT_VARS_FILE?=inventory/group_vars/all.yml
+VAULT_VARS_FILE?=inventory/group_vars/all
 VAULT_PASSWORD_FILE?=secret/vault_password
 
 .PHONY: vault_encrypt vault_decrypt
 
-vault_encrypt: $(VAULT_VARS_FILE) $(VAULT_PASSWORD_FILE)
-	ansible-vault encrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $(VAULT_VARS_FILE)
-	mv $(VAULT_VARS_FILE) $(VAULT_VARS_FILE).enc
+vault_encrypt: $(VAULT_PASSWORD_FILE)
+	for i in $(VAULT_VARS_FILE); do \
+		test -f $$i; \
+		ansible-vault encrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $$i; \
+		mv $$i $$i.enc; \
+	done
 
-vault_decrypt: $(VAULT_VARS_FILE).enc $(VAULT_PASSWORD_FILE)
-	ansible-vault decrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $(VAULT_VARS_FILE).enc
-	mv $(VAULT_VARS_FILE).enc $(VAULT_VARS_FILE)
-
-$(VAULT_VARS_FILE)%:
-	test -f $$(echo $@ | sed -e 's/.o$$//') # please for the love of god someone tell me how to fix this
+vault_decrypt: $(VAULT_PASSWORD_FILE)
+	for i in $(VAULT_VARS_FILE); do \
+		test -f $$i.enc; \
+		ansible-vault decrypt --vault-password-file=$(VAULT_PASSWORD_FILE) $$i.enc; \
+		mv $$i.enc $$i; \
+	done
 
 $(VAULT_PASSWORD_FILE):
 	ENCRYPTABLE=$(VAULT_PASSWORD_FILE) make decrypt
